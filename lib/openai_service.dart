3a3a117d59d31638a8218b9429d347cfee6e8a90
aengzu <jeon7879@naver.com>
2024-05-17 13:46:06 +0900

    feat: chat UI 수정

--- lib/openai_service.dart
+++ lib/openai_service.dart
@@ -10,7 +10,7 @@ class OpenAIService {
   static const apiUri = 'https://api.openai.com/v1/chat/completions';
 
   // TODO: 여기에 APIKEY
-  get apiKey => 'sk-proj-qYu4iPzMPVkRPZNyZxFgT3BlbkFJNwXY7v7zbMSIHIZDZe5v';
+  get apiKey => '';
   OpenAIService() {
     initializeMessages();  // 서비스 인스턴스 생성 시 메시지 초기화
   }
